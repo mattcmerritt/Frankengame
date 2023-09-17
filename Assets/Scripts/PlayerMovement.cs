a8f3810b18c3023fa1bd81cff65eb6e9cec66b8e
mattcmerritt <mattcmerritt@gmail.com>
2023-09-17 16:07:18 -0400

    imported stoppable player as player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float MoveSpeed, JumpForce;
-    [SerializeField] private bool Grounded;
+    [SerializeField] private bool started, moving;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && Grounded)
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpForce);
+            started = false; // make player press and release space again
+            moving = false; // player will not be automatically moving
+            transform.position = startPosition; // reset to starting position
         }
-        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalInput * MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // start game on first space
+            if (!started)
+            {
+                started = true;
+                moving = true;
+            }
+            // otherwise change between moving and not moving
+            else 
+            {
+                moving = !moving;
+            }
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void FixedUpdate()
     {
-        Grounded = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Grounded = false;
+        if (moving)
+        {
+            rb.velocity = Vector2.right * playerSpeed + Vector2.up * rb.velocity.y; // go forward and preserve vertical speed
+        }
+        else {
+            rb.velocity = Vector2.up * rb.velocity.y; // only keep vertical speed
+        }
     }
 }
