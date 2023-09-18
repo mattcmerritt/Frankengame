c251ee168999f8b028933a1afdfb0dd3d9e3d026
mattcmerritt <mattcmerritt@gmail.com>
2023-09-18 05:12:28 -0400

    final build

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -8,11 +8,17 @@ public class UI : MonoBehaviour
     [SerializeField] private CollectableDuration slowSeconds, fastSeconds;
     [SerializeField] private ScoreTracker score;
     [SerializeField] private TMP_Text slowText, fastText, scoreText;
+    [SerializeField] private GameObject instructions;
 
     private void Update()
     {
         slowText.text = $"Slow Down: \n{slowSeconds.GetSeconds().ToString("0.##")}s";
         fastText.text = $"Speed Up: \n{fastSeconds.GetSeconds().ToString("0.##")}s";
         scoreText.text = $"Score: \n{Mathf.FloorToInt(score.GetScore())}";
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            instructions.SetActive(false);
+        }
     }
 }
